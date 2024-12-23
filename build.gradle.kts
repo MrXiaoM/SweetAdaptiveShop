951498d881ea08a800173c82e0eb876dfbd816d7
人間工作 <coolxiaom95@gmail.com>
2024-12-23 15:20:20 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("commons-lang:commons-lang:2.6")
     implementation("com.udojava:EvalEx:2.7")
     implementation("org.jetbrains:annotations:21.0.0")
-    implementation("top.mrxiaom:PluginBase:1.1.3")
+    implementation("top.mrxiaom:PluginBase:1.1.4")
 }
 java {
     val javaVersion = JavaVersion.toVersion(targetJavaVersion)
