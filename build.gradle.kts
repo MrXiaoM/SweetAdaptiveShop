88c2191e546d2c07de624cbcefec5893c826e3c5
人間工作 <coolxiaom95@gmail.com>
2025-04-28 20:14:00 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,15 +34,15 @@ dependencies {
     compileOnly("io.lumine:Mythic:5.6.2")
     compileOnly("io.lumine:LumineUtils:1.20-SNAPSHOT")
 
-    implementation("net.kyori:adventure-api:4.17.0")
+    implementation("net.kyori:adventure-api:4.20.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.4")
-    implementation("net.kyori:adventure-text-minimessage:4.17.0")
-    implementation("de.tr7zw:item-nbt-api:2.14.2-SNAPSHOT")
+    implementation("net.kyori:adventure-text-minimessage:4.2-.0")
+    implementation("de.tr7zw:item-nbt-api:2.15.0")
     implementation("com.zaxxer:HikariCP:4.0.3")
     implementation("org.slf4j:slf4j-nop:2.0.16")
     implementation("com.udojava:EvalEx:2.7")
-    implementation("org.jetbrains:annotations:21.0.0")
-    implementation("top.mrxiaom:PluginBase:1.3.7")
+    implementation("org.jetbrains:annotations:24.0.0")
+    implementation("top.mrxiaom:PluginBase:1.4.0")
 }
 java {
     val javaVersion = JavaVersion.toVersion(targetJavaVersion)
