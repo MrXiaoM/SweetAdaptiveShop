41cf5899d691c849d390d14a223e0a874146f19f
MrXiaoM <coolxiaom95@gmail.com>
2026-01-24 18:41:58 +0800

    update 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 }
 
 group = "top.mrxiaom.sweet.adaptiveshop"
-version = "1.1.0"
+version = "1.2.0"
 
 val base = top.mrxiaom.gradle.LibraryHelper(project)
 val targetJavaVersion = 8
